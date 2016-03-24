fb2922b7742641f2e973a24bed0a898f11405454
mich5963 <mich1@yahoo.co.jp>
2016-03-24 17:08:13 +0000

    課題：オリジナル機能追加とHerokuへのデプロイ

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -9,13 +9,14 @@ class FavoritesController < ApplicationController
         @micropost = Micropost.find(params[:id])
         return redirect_to root_url if @micropost.nil?
         current_user.add_favorite(@micropost)
-        redirect_to (:back)
+        redirect_to :back
+        
     end
     
     def destroy
         @micropost = current_user.my_favorite_microposts.find(params[:id])
         current_user.remove_favorite(@micropost)
-        redirect_to (:back)
+        redirect_to :back
     end    
 
 end
